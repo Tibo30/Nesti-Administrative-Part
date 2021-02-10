53b69957bdb43e667bb75094f17c77ab04ca537b
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-02-10 19:54:31 +0100

    add if condition

--- nesti/app/controller/UserController.php
+++ nesti/app/controller/UserController.php
@@ -1,21 +1,25 @@
 <?php
-require_once(PATH_VIEW.'View.php');
-require_once(PATH_MODEL.'entities/user.php');
+require_once(PATH_VIEW . 'View.php');
+require_once(PATH_MODEL . 'entities/user.php');
 
-class UserController extends BaseController{
+class UserController extends BaseController
+{
     private $userDAO;
 
-    public function initialize(){
-        $this->users();
+    public function initialize()
+    {
+        if ($this->_url == "user") {
+            $this->users();
+        } 
     }
 
-    private function users(){
+    private function users()
+    {
         $this->userDAO = new UserDAO();
 
         $users = $this->userDAO->getUsers();
-        
+
         $this->_view = new View($this->_url);
-        $this->_data = ['users'=>$users,'url'=>$this->_url,"title"=>"User"];
-  
+        $this->_data = ['users' => $users, 'url' => $this->_url, "title" => "User"];
     }
 }
