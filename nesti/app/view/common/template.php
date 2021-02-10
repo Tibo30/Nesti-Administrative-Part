6d78c56cdb87d46be95d17ef269c254ab8da527f
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-02-10 19:54:01 +0100

    change js path

--- nesti/app/view/common/template.php
+++ nesti/app/view/common/template.php
@@ -5,7 +5,6 @@
     //echo "template / ";
 
     if ($url != 'connection') {
-        include(PATH_VIEW . 'common/header.php');
         include(PATH_VIEW . 'common/navigation.php');
     }
     if ($url == "recipe_edit" || $url == "recipe_add" || $url == "recipe_delete") {
@@ -14,14 +13,13 @@
     //echo "template2 / ";
     if (isset($view)) {
         // echo " view la / ".$view->getFile()."/";
+       
         include($view->getFile());
         //echo "view OK : ";
     } else {
         //error
     }
-
     //echo "template3 / ";
-
     //include(PATH_VIEW.'content/page_content.php');
     ?>
 
@@ -30,7 +28,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/bootstrap-table@1.18.2/dist/bootstrap-table.min.js"></script>
-    <script src="public/js/script.js"></script>
+    <script src="<?php echo BASE_URL ?>public/js/script.js"></script>
 
 </body>
 
