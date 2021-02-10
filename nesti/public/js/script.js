f310fd185ff1687d9adecdb930a0300a79d94e26
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-02-10 19:53:20 +0100

    add js on button submit recipe

--- nesti/public/js/script.js
+++ nesti/public/js/script.js
@@ -1,4 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-console.log("ok js");
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("ok js");
+
+    // -------------------------------------------js ad recipe content---------------------------------------------
+
+    // var submitNewRecipe = document.querySelector("#submitNewRecipe");
+    // if (submitNewRecipe!=null){
+    //     submitNewRecipe.addEventListener('click', function () {
+    //         var hiddenContent = document.querySelector("#hiddenContentAddRecipe");
+    //         hiddenContent.classList.add("visible");
+    //         hiddenContent.classList.remove("invisible");
+    //     });
+    // }
+    
+    var addParagraph = document.querySelector("#addParagraphNewRecipe");
+    if (addParagraph!=null){
+        addParagraph.addEventListener('click', function () {
+            var paragraph = addParagraph.previousSibling.previousSibling;
+            paragraph.innerHTML+='<textarea class="form-control mb-2" id="paragraph1" rows="5" style="resize: none;"></textarea>';
+        });
+    }
+  
 
 });
