c6c701061d83bb95d8739eccdc12a461fd577c42
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-02-10 19:53:51 +0100

    add logo

--- nesti/app/view/content/connection_content.php
+++ nesti/app/view/content/connection_content.php
@@ -1,21 +1,24 @@
 <div id="connectionPage">
+    <div id="logoNesti" class="position-absolute">
+        <img src="<?= BASE_URL ?>public/pictures/Nesti-logo.png" alt="Nesti logo">
+    </div>
     <div class="container h-100 d-flex" style="max-width:70%">
         <div class="container border my-auto bg-white" id="connectionForm">
             <div class="login-form d-flex justify-content-center">
-                <form action="recipe" method="POST">
+                <form action="" method="POST">
                     <h2 class="text-center mb-5">Log in</h2>
                     <div class="form-group">
                         <label for="emailConnection">Email</label>
                         <div class="d-flex align-items-center">
                         <i class="far fa-user mr-1"></i>
-                        <input type="email" id="emailConnection" class="form-control" placeholder="Email" required="required">
+                        <input type="email" id="emailConnection" class="form-control" placeholder="Email" required="required" name="email">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="passwordConnection">Password</label>
                         <div class="d-flex align-items-center ">
                         <i class="fas fa-lock mr-1"></i>
-                        <input type="password" id="passwordConnection" class="form-control" placeholder="Password" required="required">
+                        <input type="password" id="passwordConnection" class="form-control" placeholder="Password" required="required" name="password">
                         </div>
                     </div>
                     <div class="form-group d-flex justify-content-end">
@@ -23,11 +26,11 @@
                     </div>
             </div>
             <?php
-            if (isset($_GET['erreur'])) {
-                $err = $_GET['erreur'];
-                if ($err == 1 || $err == 2)
-                    echo "<p style='color:red'>Incorrect Username or Password</p>";
-            }
+            // if (isset($_GET['erreur'])) {
+            //     $err = $_GET['erreur'];
+            //     if ($err == 1 || $err == 2)
+            //         echo "<p style='color:red'>Incorrect Username or Password</p>";
+            // }
             ?>
         </div>
     </div>
