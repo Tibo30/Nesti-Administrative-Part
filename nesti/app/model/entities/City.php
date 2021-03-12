bd142d73c6c2f32a462cb35314c4d889f893586b
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-12 18:16:30 +0100

    add hydration city

--- nesti/app/model/entities/City.php
+++ nesti/app/model/entities/City.php
@@ -3,6 +3,14 @@ class City{
     protected $idCity;
     protected $cityName;
 
+    public function hydration($data)
+    {
+        $this->idCity = $data['id_city'];
+        $this->cityName = $data['city_name'];
+       
+        return $this;
+    }
+
     /**
      * Get the value of idCity
      */ 
