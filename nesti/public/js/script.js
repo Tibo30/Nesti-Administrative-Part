f14d339af992a921e89c4e09d8e84e7669c79841
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-12 18:14:55 +0100

    start script to add picture

--- nesti/public/js/script.js
+++ nesti/public/js/script.js
@@ -28,11 +28,69 @@ document.addEventListener("DOMContentLoaded", function () {
     //     })
     // })
 
-   
+
 
 
 });
+
+
 function deleteIngredient(){
     var parent = event.target.parentNode.parentNode;
     parent.remove();
 }
+
+   // -------------------------------------------js article edit content --------------------------------------------- //
+
+   function editArticlePicture(){
+    console.log("test");
+    deleteTag(id, id_tag).then((response) => {
+        if (response) {
+            if (response.success && response.csrf_token) {
+                alert('Suppresion ok');
+                // Raffraichissement du CSRF
+                csrf.setAttribute('value', response.csrf_token);
+                this.parentElement.remove();
+            }
+        }
+    });
+}
+
+ /**
+     * Requete Ajax pour supprimer un tag d'une recette
+     * @param {int} id_recipe
+     * @param {int} id_tag
+     * @returns mixed
+     */
+  async function deleteTag( id_recipe, id_tag) {
+    // Requete
+    var myHeaders = new Headers();
+    
+    
+
+    let formData = new FormData();
+    formData.append('id_recipe', id_recipe);
+    formData.append('id_tag', id_tag);
+    formData.append(csrf.name, csrf.value);
+
+    var myInit = {method: 'POST',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        body: formData
+    };
+    let response = await fetch(ROOT + '/tag/delete', myInit);
+    try {
+        if (response.ok) {
+            return await response.json();
+        } else {
+            return false;
+        }
+    } catch (e) {
+        console.error(e.message);
+    }
+
+
+}
+
+
+
