711f0e8a1768fffdd3abfee757e706efa2d72641
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-13 17:16:34 +0100

    update

--- nesti/public/js/script.js
+++ nesti/public/js/script.js
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
     //         hiddenContent.classList.remove("invisible");
     //     });
     // }
-    
+
     var addParagraph = document.querySelector("#addParagraphNewRecipe");
-    if (addParagraph!=null){
+    if (addParagraph != null) {
         addParagraph.addEventListener('click', function () {
             var paragraph = addParagraph.previousSibling.previousSibling;
-            paragraph.innerHTML+='<textarea class="form-control mb-2" id="paragraph1" rows="5" style="resize: none;"></textarea>';
+            paragraph.innerHTML += '<textarea class="form-control mb-2" id="paragraph1" rows="5" style="resize: none;"></textarea>';
         });
     }
 
@@ -28,69 +28,38 @@ document.addEventListener("DOMContentLoaded", function () {
     //     })
     // })
 
+// -------------------------------------------js article all content --------------------------------------------- /
 
+const deleteArticles = document.querySelectorAll("#allArticleDelete");
+
+deleteArticles.forEach(element=>function(){
+    element.addEventListener('click',function (event){
+        event.preventDefault();
+        console.log("test");
+        event.preventDefault();
+        alert("ok");
+    })
+})
 
 
 });
 
 
-function deleteIngredient(){
+function deleteIngredient() {
     var parent = event.target.parentNode.parentNode;
     parent.remove();
 }
 
-   // -------------------------------------------js article edit content --------------------------------------------- //
-
-   function editArticlePicture(){
-    console.log("test");
-    deleteTag(id, id_tag).then((response) => {
-        if (response) {
-            if (response.success && response.csrf_token) {
-                alert('Suppresion ok');
-                // Raffraichissement du CSRF
-                csrf.setAttribute('value', response.csrf_token);
-                this.parentElement.remove();
-            }
-        }
-    });
-}
-
- /**
-     * Requete Ajax pour supprimer un tag d'une recette
-     * @param {int} id_recipe
-     * @param {int} id_tag
-     * @returns mixed
-     */
-  async function deleteTag( id_recipe, id_tag) {
-    // Requete
-    var myHeaders = new Headers();
-    
-    
-
-    let formData = new FormData();
-    formData.append('id_recipe', id_recipe);
-    formData.append('id_tag', id_tag);
-    formData.append(csrf.name, csrf.value);
-
-    var myInit = {method: 'POST',
-        headers: myHeaders,
-        mode: 'cors',
-        cache: 'default',
-        body: formData
-    };
-    let response = await fetch(ROOT + '/tag/delete', myInit);
-    try {
-        if (response.ok) {
-            return await response.json();
-        } else {
-            return false;
-        }
-    } catch (e) {
-        console.error(e.message);
-    }
 
 
-}
+
+// -------------------------------------------js article edit content --------------------------------------------- //
+// const formImage = document.querySelector("#formImage");
+// formImage.addEventListener('submit',function(e){
+//     console.log(this);
+//     event.preventDefault();
+// })
+
 
 
 
