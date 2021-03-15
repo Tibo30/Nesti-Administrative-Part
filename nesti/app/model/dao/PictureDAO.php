a02ae5d488a252099cf320d60809e4a659586490
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-15 20:43:24 +0100

    add query insert picture

--- nesti/app/model/dao/PictureDAO.php
+++ nesti/app/model/dao/PictureDAO.php
@@ -11,4 +11,13 @@ class PictureDAO extends ModelDAO
         $req->closeCursor(); // release the server connection so it's possible to do other query
         return $pictureObject;
     }
+
+    public function insertPicture($picture)
+    {
+
+        $req = self::$_bdd->prepare('INSERT INTO pictures (creation_date, name, extension) VALUES (CURRENT_TIMESTAMP, :name, :extension)');
+        $req->execute(array("name"=>$picture->getName(),"extension"=>$picture->getExtension()));
+        $last_id = self::$_bdd->lastInsertId();
+        return $last_id;
+    }
 }
