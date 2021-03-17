5983d6b6083d9928573006c5dad7fb833b623a42
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-17 19:57:25 +0100

    comment script

--- nesti/public/js/script.js
+++ nesti/public/js/script.js
@@ -1,64 +1,64 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("ok js");
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log("ok js");
 
-    // -------------------------------------------js ad recipe content---------------------------------------------
+//     // -------------------------------------------js ad recipe content---------------------------------------------
 
-    // var submitNewRecipe = document.querySelector("#submitNewRecipe");
-    // if (submitNewRecipe!=null){
-    //     submitNewRecipe.addEventListener('click', function () {
-    //         var hiddenContent = document.querySelector("#hiddenContentAddRecipe");
-    //         hiddenContent.classList.add("visible");
-    //         hiddenContent.classList.remove("invisible");
-    //     });
-    // }
+//     // var submitNewRecipe = document.querySelector("#submitNewRecipe");
+//     // if (submitNewRecipe!=null){
+//     //     submitNewRecipe.addEventListener('click', function () {
+//     //         var hiddenContent = document.querySelector("#hiddenContentAddRecipe");
+//     //         hiddenContent.classList.add("visible");
+//     //         hiddenContent.classList.remove("invisible");
+//     //     });
+//     // }
 
-    var addParagraph = document.querySelector("#addParagraphNewRecipe");
-    if (addParagraph != null) {
-        addParagraph.addEventListener('click', function () {
-            var paragraph = addParagraph.previousSibling.previousSibling;
-            paragraph.innerHTML += '<textarea class="form-control mb-2" id="paragraph1" rows="5" style="resize: none;"></textarea>';
-        });
-    }
+//     var addParagraph = document.querySelector("#addParagraphNewRecipe");
+//     if (addParagraph != null) {
+//         addParagraph.addEventListener('click', function () {
+//             var paragraph = addParagraph.previousSibling.previousSibling;
+//             paragraph.innerHTML += '<textarea class="form-control mb-2" id="paragraph1" rows="5" style="resize: none;"></textarea>';
+//         });
+//     }
 
-    // var deleteIngredient = document.querySelectorAll(".btn-remove-ingredients");
-    // deleteIngredient.forEach(element=>{
-    //     element.addEventListener('click',function(){
-    //         var parent = element.parentNode.parentNode;
-    //         parent.remove();
-    //     })
-    // })
+//     // var deleteIngredient = document.querySelectorAll(".btn-remove-ingredients");
+//     // deleteIngredient.forEach(element=>{
+//     //     element.addEventListener('click',function(){
+//     //         var parent = element.parentNode.parentNode;
+//     //         parent.remove();
+//     //     })
+//     // })
 
-// -------------------------------------------js article all content --------------------------------------------- /
+// // -------------------------------------------js article all content --------------------------------------------- /
 
-const deleteArticles = document.querySelectorAll("#allArticleDelete");
+// const deleteArticles = document.querySelectorAll("#allArticleDelete");
 
-deleteArticles.forEach(element=>function(){
-    element.addEventListener('click',function (event){
-        event.preventDefault();
-        console.log("test");
-        event.preventDefault();
-        alert("ok");
-    })
-})
-
-
-});
-
-
-function deleteIngredient() {
-    var parent = event.target.parentNode.parentNode;
-    parent.remove();
-}
-
-
-
-
-// -------------------------------------------js article edit content --------------------------------------------- //
-// const formImage = document.querySelector("#formImage");
-// formImage.addEventListener('submit',function(e){
-//     console.log(this);
-//     event.preventDefault();
+// deleteArticles.forEach(element=>function(){
+//     element.addEventListener('click',function (event){
+//         event.preventDefault();
+//         console.log("test");
+//         event.preventDefault();
+//         alert("ok");
+//     })
 // })
+
+
+// });
+
+
+// function deleteIngredient() {
+//     var parent = event.target.parentNode.parentNode;
+//     parent.remove();
+// }
+
+
+
+
+// // -------------------------------------------js article edit content --------------------------------------------- //
+// // const formImage = document.querySelector("#formImage");
+// // formImage.addEventListener('submit',function(e){
+// //     console.log(this);
+// //     event.preventDefault();
+// // })
 
 
 
