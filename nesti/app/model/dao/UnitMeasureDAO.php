097f4e99aece0afcbfb03052163e6945b4e4f88f
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-26 15:01:27 +0100

    add queries

--- nesti/app/model/dao/UnitMeasureDAO.php
+++ nesti/app/model/dao/UnitMeasureDAO.php
@@ -12,4 +12,26 @@ class UnitMeasureDAO extends ModelDAO
         $req->closeCursor(); // release the server connection so it's possible to do other query
         return $unitObject;
     }
+
+    public function getUnitMeasureByName($unitName)
+    {
+        $req = self::$_bdd->prepare('SELECT * FROM unit_measures WHERE name=:name');
+        $req->execute(array("name" => $unitName));
+        $unitObject = new UnitMeasure();
+        if ($data = $req->fetch()) {
+            $unitObject->hydration($data);
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $unitObject;
+    }
+
+    public function createUnitMeasure($unitName)
+    {
+        $req = self::$_bdd->prepare('INSERT INTO unit_measures (name) VALUES (:name)');
+        $req->execute(array("name" => $unitName));
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        $last_id = self::$_bdd->lastInsertId();
+        return $last_id;
+    }
+
 }
