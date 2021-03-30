e9df84c42ddd6808f15e2328c04eb9b5f758c107
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-03-30 21:04:34 +0200

    add query

--- nesti/app/model/dao/LotDAO.php
+++ nesti/app/model/dao/LotDAO.php
@@ -9,11 +9,23 @@ class LotDAO extends ModelDAO
         if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
             foreach ($data as $row) {
                 $lot=new Lot();
-                $var[] = $lot->hydration($row);;
+                $var[] = $lot->hydration($row);
             }
         }
         $req->closeCursor(); // release the server connection so it's possible to do other query
         return $var;
+    }
+
+    public function getLot($idArticle, $idOrder)
+    {
+        $req = self::$_bdd->prepare('SELECT * FROM lots WHERE id_article=:idArticle AND ref_order=:idOrder');
+        $req->execute(array("idArticle" => $idArticle,"idOrder" => $idOrder));
+        $lot=new Lot();
+        if ($data = $req->fetch()) {
+            $lot->hydration($data);
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $lot;
     }
     
 }
