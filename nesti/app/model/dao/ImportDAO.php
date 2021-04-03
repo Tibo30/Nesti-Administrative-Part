e091f8ec03235367d49eb24d1cd6779c11808206
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-03 13:53:45 +0200

    add get last import and imports user queries

--- nesti/app/model/dao/ImportDAO.php
+++ nesti/app/model/dao/ImportDAO.php
@@ -13,10 +13,37 @@ class ImportDAO extends ModelDAO
         return $importObject;
     }
 
+    public function getLastImportUser($idUser)
+    {
+        $req = self::$_bdd->prepare('SELECT * FROM import WHERE id_admin=:id ORDER BY import_date DESC LIMIT 1');
+        $req->execute(array("id" => $idUser));
+        $importObject = new Import();
+        if ($data = $req->fetch()) {
+            $importObject->hydration($data);
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $importObject;
+    }
+
     public function getImports()
     {
         $req = self::$_bdd->prepare('SELECT * FROM import');
         $req->execute();
+        if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
+            foreach ($data as $row) {
+                $import = new Import();
+                $import->hydration($row);
+                $var[] = $import;
+            }
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $var;
+    }
+
+    public function getImportsUser($idUser)
+    {
+        $req = self::$_bdd->prepare('SELECT * FROM import WHERE id_admin=:id ');
+        $req->execute(array("id" => $idUser));
         if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
             foreach ($data as $row) {
                 $import = new Import();
