2536b247e60f1d33d102ea0c2539f52d44a00786
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-03 13:53:32 +0200

    add get comments user query

---
+++ nesti/app/model/dao/CommentsDAO.php
@@ -0,0 +1,20 @@
+<?php
+class CommentsDAO extends ModelDAO
+{
+    // get all comments for a moderator
+    public function getComments($idUser)
+    {
+        $var=[];
+        $req = self::$_bdd->prepare('SELECT * FROM comments WHERE id_moderator=:id');
+        $req->execute(array("id" => $idUser));
+        if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
+            foreach ($data as $row) {
+                $comment = new Comments();
+                $comment->hydration($row);
+                $var[] = $comment;
+            }
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $var;
+    }
+}
