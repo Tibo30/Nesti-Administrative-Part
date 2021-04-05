aafffc6ec0953b8aae3ca05e0079a3107a360ced
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-05 19:13:07 +0200

    update with access

--- nesti/app/view/content/statistic_content.php
+++ nesti/app/view/content/statistic_content.php
@@ -1,1 +1,11 @@
-<h1>Stats</h1>
+<?php if (array_search("admin", $_SESSION["roles"]) === false) {
+
+?>
+    <div class="container">
+        <h2 class="titleAccessForbidden">Access forbidden</h2>
+        <p class="textAccessForbidden">You don't have the rights to access this page</p>
+    </div>
+<?php } else { ?>
+    <h1>Stats</h1>
+
+<?php } ?>
