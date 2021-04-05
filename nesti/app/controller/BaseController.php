ade164c2843481f05bc427c16bb372f50b52ba6d
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-05 19:07:21 +0200

    update

--- nesti/app/controller/BaseController.php
+++ nesti/app/controller/BaseController.php
@@ -1,36 +1,37 @@
 <?php
-require_once(PATH_VIEW.'View.php');
+require_once(PATH_VIEW . 'View.php');
 
-abstract class BaseController{
+abstract class BaseController
+{
     protected View $_view;
     protected $_url;
-    protected $_data=[];
+    protected $_data = [];
 
-    public function __construct($url){
-        
-        if (isset($_GET["url"]) && ((($url[0]!="recipe")&&($url[0]!="article")&&($url[0]!="user")&&($url[0]!="statistic"))||($url[0]=="statistic" && count($url)>1))){
-           // echo $url;
-            throw new Exception ('Page introuvable');
+    public function __construct($url)
+    {
+
+        if (isset($_GET["url"]) && ((($url[0] != "connection") && ($url[0] != "recipe") && ($url[0] != "article") && ($url[0] != "user") && ($url[0] != "statistic")) || ($url[0] == "statistic" && count($url) > 1))) {
+            throw new Exception('Page introuvable');
         } else {
-            if (($url[0]=="recipe"|| $url[0]=="article"|| $url[0]=="user") && count($url)>1){
-                $this->_url=$url[0]."_".$url[1];
-               //echo "url : ".$this->_url;
+            if (($url[0] == "recipe" || $url[0] == "article" || $url[0] == "user" || $url[0] == "connection") && count($url) > 1) {
+                $this->_url = $url[0] . "_" . $url[1];
             } else {
-                $this->_url= $url[0];
+                $this->_url = $url[0];
             }
-            //echo "base controller / ";
             $this->initialize();
         }
     }
-// return a view from View class
-    public function getView():View{
-        if(isset($this->_view)){
+    // return a view from View class
+    public function getView(): View
+    {
+        if (isset($this->_view)) {
             return $this->_view;
         }
     }
 
-    public function getData(){
-        if (isset($this->_data) &&!empty($this->_data)){
+    public function getData()
+    {
+        if (isset($this->_data) && !empty($this->_data)) {
             return $this->_data;
         }
     }
