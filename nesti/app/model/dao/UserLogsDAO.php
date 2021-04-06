b181d3155a482b71692b06065d0d178c2b226392
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-06 20:00:26 +0200

    add get logs and get logs for user

--- nesti/app/model/dao/UserLogsDAO.php
+++ nesti/app/model/dao/UserLogsDAO.php
@@ -12,4 +12,35 @@ class UserLogsDAO extends ModelDAO
         $req->closeCursor(); // release the server connection so it's possible to do other query
         return $userlog;
     }
+
+    public function getLogs(){
+        $var = [];
+        $req = self::$_bdd->prepare('SELECT * FROM user_logs');
+        $req->execute();
+        if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
+            foreach ($data as $row) {
+                $userLog = new UserLog();
+                $userLog->hydration($row);
+                $var[] = $userLog;
+            }
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $var;
+    }
+
+    public function getLogsUser($idUser){
+        $var = [];
+        $req = self::$_bdd->prepare('SELECT * FROM user_logs WHERE id_users=:id');
+        $req->execute(array("id" => $idUser));
+        if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
+            foreach ($data as $row) {
+                $userLog = new UserLog();
+                $userLog->hydration($row);
+                $var[] = $userLog;
+            }
+        }
+        $req->closeCursor(); // release the server connection so it's possible to do other query
+        return $var;
+    }
+
 }
