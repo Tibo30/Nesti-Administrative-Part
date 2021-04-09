df9cc45578cb2ae9cc041a1bb2da4c6ae3fc2863
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-09 19:26:21 +0200

    update query

--- nesti/app/model/dao/ArticlePriceDAO.php
+++ nesti/app/model/dao/ArticlePriceDAO.php
@@ -1,15 +1,31 @@
 <?php
 class ArticlePriceDAO extends ModelDAO
 {
+    // public function getPrice($idArticle)
+    // {
+    //     $req = self::$_bdd->prepare('SELECT * FROM article_price WHERE id_article=:id');
+    //     $req->execute(array("id" => $idArticle));
+    //     $priceObject = new ArticlePrice();
+    //     if ($data = $req->fetch()) {
+    //         $priceObject->hydration($data);
+    //     }
+    //     $req->closeCursor(); // release the server connection so it's possible to do other query
+    //     return $priceObject;
+    // }
+
     public function getPrice($idArticle)
     {
-        $req = self::$_bdd->prepare('SELECT * FROM article_price WHERE id_article=:id');
+        $var = [];
+        $req = self::$_bdd->prepare('SELECT * FROM article_price WHERE id_article=:id ORDER BY application_date DESC');
         $req->execute(array("id" => $idArticle));
-        $priceObject = new ArticlePrice();
-        if ($data = $req->fetch()) {
-            $priceObject->hydration($data);
+        if ($data = $req->fetchAll(PDO::FETCH_ASSOC)) {
+            foreach ($data as $row) {
+                $priceObject=new ArticlePrice();
+                $var[] = $priceObject->hydration($row);;
+            }
         }
+        $lastPriceObject=$var[0];
         $req->closeCursor(); // release the server connection so it's possible to do other query
-        return $priceObject;
+        return $lastPriceObject;
     }
 }
