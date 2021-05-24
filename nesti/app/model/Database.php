a79c02bc318bb6b987c8925eb970df9518d2eeb4
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-05-24 15:02:50 +0200

    add comments

--- nesti/app/model/Database.php
+++ nesti/app/model/Database.php
@@ -4,17 +4,24 @@ class Database
 
     protected static $_bdd=null;
  
-    //instancing bdd connection
+    /**
+     * instancing bdd connection
+     */
     public static function setBdd()
     {
         self::$_bdd = new PDO(DSN, USERNAME, PWD, [PDO::ATTR_PERSISTENT=>true]);
     }
 
-    // get the bdd connection
+    /**
+     * get the bdd connection
+     */
     public function getBdd(){
             return self::$_bdd;
     }
 
+    /**
+     * Disconnect
+     */
     public function disconnect()
     {
         self::$_bdd = null;
